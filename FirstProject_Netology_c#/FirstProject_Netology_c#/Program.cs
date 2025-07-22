11a7a7523fddcb026d4579e77c4e0c5113a6a6ca
Sergey Rutsky <rutsky.s.v@gmail.com>
2025-07-23 00:34:08 +0300

    finish arrive

--- FirstProject_Netology_c#/FirstProject_Netology_c#/Program.cs
+++ FirstProject_Netology_c#/FirstProject_Netology_c#/Program.cs
@@ -9,7 +9,7 @@ namespace FirstJob
     {
 
         static void Main(string[] args)
-        {
+        {//Задача А
             //выполнение первого задания
             int[] fibonachi = new int[8] {0,1,1,2,3,5,8,13};
 
@@ -35,14 +35,33 @@ namespace FirstJob
             jagged[2][2] = Math.Log10(100);
             jagged[2][3] = Math.Log10(1000);
 
+            //Задача Б
+            //пятое задание
+            int[] array = new int[5] { 1, 2, 3, 4, 5 };
+            int[] array2 = new int[] { 7, 8, 9, 10, 11, 12, 13 };
+           Array.Copy(array, 0, array2, 0, 3);// если правильно понял, то последовательность параметров: исходный массив(откуда), откуда начинаем копирование, массив куда копируем, куда вставляем, сколько элементов
+            foreach(int i in array2)
+            {
+                //Console.WriteLine(i);
+            }
+            //шестое
+            int NewDigit = array.Length * 2;
+            Array.Resize(ref array, NewDigit);  //или попроще, зная размер массива Array.Resize(ref array, 10);
+            /*foreach(int i in array)
+            {
+                Console.WriteLine(i);
+            }*/
 
 
 
-            
-            
-            
-            
-            
+
+
+
+
+
+
+
+
 
         }
     }
