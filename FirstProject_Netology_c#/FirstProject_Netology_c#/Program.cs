3e111e966487aaaf1f3c4f6c3e63e4514066d723
Sergey Rutsky <rutsky.s.v@gmail.com>
2025-08-14 00:18:28 +0300

    Collection

--- FirstProject_Netology_c#/FirstProject_Netology_c#/Program.cs
+++ FirstProject_Netology_c#/FirstProject_Netology_c#/Program.cs
@@ -2,67 +2,145 @@
 using System.Text.RegularExpressions;
 using System.Text;
 using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Globalization;
 
 namespace FirstJob
 {
-    class Program
+    internal class Program
     {
 
         static void Main(string[] args)
-        {//Задача А
-            //выполнение первого задания
-            int[] fibonachi = new int[8] {0,1,1,2,3,5,8,13};
-
-            //выполнение второго задания
-            string[] month = new string[12] { "January", "February", "March","April","May","June","July","August","September","October","November","December"};
-
-            //третье задание
-            int[,] matrix = new int[3, 3] { { 2, 3, 4 }, { 4, 9, 16 }, { 8, 27, 64 } };
-
-            //четвертое задание 
-            double[][] jagged = new double[3][]; //создаем массив с подмассивами */
-            jagged[0] = new double[5];// первый подмассив, и загоняем его в первую строку
-            for(int i = 0; i < 5; i++)
+        {
+            int digit;
+            do
             {
-                jagged[0][i] = i + 1;
+                Console.WriteLine("Доброго дня! для продолжения, введите номер задачи: 1,2 или 3");
+                if (int.TryParse(Console.ReadLine(), out digit)&& (digit>=1&&digit<=3))
+                {
+                    break;
+                    
+                }
+                Console.WriteLine("Ошибка ввода данных!... скоро запустимся заново");
+            } while (true);
+
+            Console.WriteLine("Ожидаем запуска программы");
+            switch (digit)
+            {
+                case 1: CheckTaskFirst(); break;
+                    case 2: CheckTaskSecond(); break;
+                    case 3: CheckTaskThird(); break;
             }
-            jagged[1] = new double[2]; //загоняем во вторую строку второй массив из констант
-            jagged[1][0] = Math.E;
-            jagged[1][1] = Math.PI;
-            jagged[2] = new double[4];//загоняем массив с логаритмами
-            jagged[2][0] = Math.Log10(1);
-            jagged[2][1] = Math.Log10(10);
-            jagged[2][2] = Math.Log10(100);
-            jagged[2][3] = Math.Log10(1000);
+        }
 
-            //Задача Б
-            //пятое задание
-            int[] array = new int[5] { 1, 2, 3, 4, 5 };
-            int[] array2 = new int[] { 7, 8, 9, 10, 11, 12, 13 };
-           Array.Copy(array, 0, array2, 0, 3);// если правильно понял, то последовательность параметров: исходный массив(откуда), откуда начинаем копирование, массив куда копируем, куда вставляем, сколько элементов
-            foreach(int i in array2)
-            {
-                //Console.WriteLine(i);
+        private class One
+        {
+            private readonly List<string> _list = new List<string>() { "first", "second", "third" };
+            public void TaskLoop()
+            { 
+             
+                while (true)
+                {
+                    Console.WriteLine("Пожалуйста, введите новую строчку для добавления ее в список");
+                    _list.Add(Console.ReadLine());
+                    Console.WriteLine("ОТлично, успешно добавлено в список, на данный момент список состоит из следующий элементов:");
+                    Console.WriteLine(string.Join(",", _list));
+                    Console.WriteLine("Добавьте еще одну строку в список, ее запихнем в середину");
+                    _list.Insert(_list.Count/2, Console.ReadLine());
+                    Console.WriteLine("Отлично, теперь список выглядит так:" + string.Join(",", _list));
+                    Console.WriteLine("Если хотите завершить добавление, введите \"stop\". Для продолжения введите любой символ.");
+                    string input = Console.ReadLine();
+                    if(input=="stop")
+                    {
+                        Console.WriteLine("Спасибо, работа завершена, надеюсь, выполнена верно.");
+                        break;
+                    }
+                }
             }
-            //шестое
-            int NewDigit = array.Length * 2;
-            Array.Resize(ref array, NewDigit);  //или попроще, зная размер массива Array.Resize(ref array, 10);
-            /*foreach(int i in array)
-            {
-                Console.WriteLine(i);
-            }*/
-
-
-
-
-
-
-
-
-
-
-
 
         }
+        private class Two
+        {
+            private readonly Dictionary<string, int> _journal = new Dictionary<string, int>();
+            public void TaskLoop()
+            {
+                while (true)
+                {
+                    Console.WriteLine("Поиграем в преподавателя. будем вводить Фамилию, а потом ставить его оценку в журнал...");
+                    Console.WriteLine("Введите фамилию ученика:");
+                    string family = Console.ReadLine();
+                    do
+                    {
+                        Console.WriteLine("а теперь введите оценку, от 2 до 5");
+                        if (int.TryParse(Console.ReadLine(), out int digit) && (digit >= 2 && digit <= 5))
+                        {
+                            _journal[family] = digit;//_journal.Add(family,digit)
+
+                            break;
+
+                        }
+                        Console.WriteLine("некорректные данные, попробуй снова");
+                    } while (true);
+                    Console.WriteLine($"Студент {family} успешно добавлен в журнал");
+
+                    do
+                    {
+                        Console.WriteLine("Давай проверим работоспособность нашего журнала. ВВеди фамилию студента:");
+                        string name = Console.ReadLine();
+                        if (_journal.ContainsKey(name))
+                        {
+                            Console.WriteLine($"у студента {name} оценка {_journal[name]}");
+                            break;
+                        }
+                        Console.WriteLine("УУУпс, такого нет, попробуй еще раз.");
+                    } while (true);
+
+
+                    Console.WriteLine("Если хотите завершить добавление, введите \"stop\". Если хотите перед выходом просмотреть весь журнал, введите " +
+                        "\"J\". Для продолжения введите любой символ.");
+                    string input = Console.ReadLine();
+                    if (input == "stop")
+                    {
+                        Console.WriteLine("Спасибо, работа завершена, надеюсь, выполнена верно.");
+                        break;
+                    }
+                    if (input == "J")
+                    {
+                        foreach (var people in _journal)
+                        {
+                            Console.WriteLine($"{people.Key}: {people.Value}");
+                            continue;
+                        }
+                    } }
+            
+
+
+            }
+
+        }
+        private class Three
+        {
+            public void TaskLoop()
+            {
+
+            }
+
+        }
+        private static void CheckTaskFirst()
+        {
+            var listTask = new One();
+            listTask.TaskLoop();
+        }
+        private static void CheckTaskSecond()
+        {
+            var listTask = new Two();
+            listTask.TaskLoop();
+        }
+        private static void CheckTaskThird()
+        {
+            var listTask = new Three();
+            listTask.TaskLoop();
+        }
     }
+        
+    
 }
